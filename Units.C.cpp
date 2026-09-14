520136debc060f408e3e53e36e28b585c9f7ec51
kiniti703 <bildadgachau7@gmail.com>
2026-09-14 09:43:21 +0300

    Add files via upload

---
+++ Units.C.cpp
@@ -0,0 +1,25 @@
+//Single line comment
+//Program to display academic units offered this semester
+/*
+Author: Bildad Kiniti
+Registration number: BCS-03-0135/2026
+Description: BCS units program.
+Date: 10/09/2026
+Version.1
+*/
+
+#include <stdio.h>// scanf(), printf()
+int main() {
+	printf("============BSC UNITS=========\n");
+	printf("BCE 111: Computational logic.\n");
+	printf("BCU 111: Communication skills.\n");
+	printf("BCU 112: Introduction to computers and internet.\n");
+	printf("BCU 113: Health and wellness management.\n");
+	printf("BSD 111: Structured programming and algorithms.\n");
+	printf("MAT 111: Basic mathematics.\n");
+	printf("PHY 111: Mathematical physics.\n");
+
+	
+		
+	return 0;
+}
